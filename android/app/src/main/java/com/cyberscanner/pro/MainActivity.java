0adcd235e9f97c30c59ccc89de67cf307f40c07e
fer000666ponce-moreno <fer.000666ponce@gmail.com>
2026-05-09 20:40:35 -0600

    fix MainActivity remove Capacitor

--- android/app/src/main/java/com/cyberscanner/pro/MainActivity.java
+++ android/app/src/main/java/com/cyberscanner/pro/MainActivity.java
@@ -1,7 +1,12 @@
 package com.cyberscanner.pro;
 
 import android.os.Bundle;
-import com.getcapacitor.BridgeActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends BridgeActivity {
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
