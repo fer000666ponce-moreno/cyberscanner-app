73ffdd336e4a22f6c34ce9cc6565ebd0bced5edf
fer000666ponce-moreno <fer.000666ponce@gmail.com>
2026-05-10 00:42:26 -0600

    fix blank screen webview

--- android/app/src/main/java/com/cyberscanner/app/MainActivity.java
+++ android/app/src/main/java/com/cyberscanner/app/MainActivity.java
@@ -2,10 +2,21 @@ package com.cyberscanner.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("file:///android_asset/public/index.html");
     }
 }
