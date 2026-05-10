0e0618f1cc9855878fcbfcdaa2ac95b4b3c181da
fer000666ponce-moreno <fer.000666ponce@gmail.com>
2026-05-10 00:19:41 -0600

    update

--- android/app/src/main/java/com/cyberscanner/pro/MainActivity.java
+++ android/app/src/main/java/com/cyberscanner/pro/MainActivity.java
@@ -1,12 +1,6 @@
 package com.cyberscanner.pro;
 
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+public class MainActivity extends BridgeActivity {
 }
